483222922a444b0b9f4e10a5dc30585341b08022
Marcolino5 <102527399+Marcolino5@users.noreply.github.com>
2025-09-29 00:40:33 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(process.env.PORT ?? 3001, '0.0.0.0');
-  console.log(`🚀 Server running on http://0.0.0.0:${port}`);
+  console.log(`🚀 Server running on http://0.0.0.0:${process.env.PORT}`);
 }
 void bootstrap();
